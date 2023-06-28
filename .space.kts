2435f7286fcaab9f9a5858c127ef43341a335663
Lamberto Basti <lamberto.basti@jetbrains.com>
2023-06-28 15:12:19 +0200

    Refactor branch inclusion in git push job
    The branch inclusion method in the 'Publish Snapshots' job has been refactored. The `.unaryPlus()` operator was previously applied directly to each branch name, but this caused issues when trying to include multiple branches. Now, a new infrastructure has been added to better handle branch inclusions. Two new functions 'RefSpecs.branch' and 'PathFilter.branch' are introduced to precisely filter the incoming pushes to the "lamberto.basti/dev" branch thereby improving code efficiency and readability.

--- .space.kts
+++ .space.kts
@@ -1,17 +1,17 @@
-fun PathFilter.include(vararg branches: String) {
-    branches.forEach { branch ->
-        branch.unaryPlus()
-    }
-}
-
 job("Publish Snapshots") {
+    git {
+        refSpec {
+            branch("lamberto.basti/dev")
+        }
+    }
     startOn {
         gitPush {
             pathFilter {
-                include("lamberto.basti/dev")
+                branch("lamberto.basti/dev")
             }
         }
     }
+
     gradlew("eclipse-temurin:17", "publish") {
         env["IS_SNAPSHOT"] = "true"
         env["MAVEN_SPACE_USERNAME"] = "{{ project:jetbrains_team_registry_username }}"
@@ -20,3 +20,9 @@ job("Publish Snapshots") {
         env["CI"] = "true"
     }
 }
+
+fun RefSpecs.branch(name: String) =
+    "/refs/head/${name.removePrefix("/")}".unaryPlus()
+
+fun PathFilter.branch(name: String) =
+    "/refs/head/${name.removePrefix("/")}".unaryPlus()
