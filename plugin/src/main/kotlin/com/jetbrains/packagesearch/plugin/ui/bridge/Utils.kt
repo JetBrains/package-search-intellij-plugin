2940b2f320c08a10b71ad3b605c273e6a3dab07b
Lamberto Basti <basti.lamberto@gmail.com>
2023-09-07 11:40:59 +0200

    Added PackageSearchResourceLoader

--- plugin/src/main/kotlin/com/jetbrains/packagesearch/plugin/ui/bridge/Utils.kt
+++ plugin/src/main/kotlin/com/jetbrains/packagesearch/plugin/ui/bridge/Utils.kt
@@ -1,13 +1,20 @@
 package com.jetbrains.packagesearch.plugin.ui.bridge
 
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.ResourceLoader
+import com.intellij.icons.AllIcons.Icons
+import com.intellij.ide.ui.laf.IdeaLaf
+import com.jetbrains.packagesearch.plugin.PackageSearchToolWindowFactory
 import com.jetbrains.packagesearch.plugin.core.data.PackageSearchModule
 import java.awt.Desktop
+import java.io.InputStream
 import java.net.URI
+import java.util.jar.JarFile
 import javax.swing.UIDefaults
 import javax.swing.UIManager
 import org.jetbrains.jewel.foundation.tree.TreeGeneratorScope
 import org.jetbrains.jewel.foundation.tree.buildTree
+import org.jetbrains.jewel.themes.intui.standalone.IntUiDefaultResourceLoader
 
 fun java.awt.Color.toComposeColor(): Color {
     return Color(red, green, blue, alpha)
@@ -66,3 +73,48 @@ fun isLightTheme(): Boolean {
 }
 
 private fun Color.getBrightness() = (red * 299 + green * 587 + blue * 114) / 1000
+
+fun getJarPath(klass: Class<*>): String? {
+        val className = klass.name.replace('.', '/') + ".class"
+        val classPath = klass.classLoader.getResource(className)?.toString() ?: return null
+        if (!classPath.startsWith("jar")) {
+                // Class not from a JAR
+                return null
+            }
+        return classPath.substringBefore("!").removePrefix("jar:file:")
+    }
+
+fun extractFileFromJar(jarPath: String, filePath: String) =
+        JarFile(jarPath).use { jar ->
+                jar.getEntry(filePath)?.let { entry ->
+                        jar.getInputStream(entry).use { it.readBytes().inputStream() }
+                    }
+            }
+
+inline fun <reified T> getJarPath(): String? = getJarPath(T::class.java)
+
+class RawJarResourceLoader(private val jars: List<String>) : ResourceLoader {
+        override fun load(resourcePath: String): InputStream =
+            jars.firstNotNullOfOrNull { extractFileFromJar(it, resourcePath) }
+                ?: error("Resource $resourcePath not found in jars $jars")
+    }
+
+object PackageSearchResourceLoader : ResourceLoader {
+
+    private val rawJarResourceLoader = RawJarResourceLoader(
+        jars = buildList {
+            getJarPath<PackageSearchModule>()?.let { add(it) }
+            getJarPath<PackageSearchToolWindowFactory>()?.let { add(it) }
+            getJarPath<Icons>()?.let { add(it) }
+            getJarPath<IdeaLaf>()?.let { add(it) }
+        }
+    )
+
+    override fun load(resourcePath: String) =
+        IntUiDefaultResourceLoader.loadOrNull(resourcePath)
+            ?: rawJarResourceLoader.load(resourcePath)
+
+}
+
+
+
