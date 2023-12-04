8904c6dc760e98e634b036b43d798f03711a24f0
Lamberto Basti <basti.lamberto@gmail.com>
2023-12-04 19:17:24 +0100

    Updated publication logic for snapshots

--- buildSrc/src/main/kotlin/org/jetbrains/packagesearch/gradle/ConfigureGradleIntellijPlugin.kt
+++ buildSrc/src/main/kotlin/org/jetbrains/packagesearch/gradle/ConfigureGradleIntellijPlugin.kt
@@ -15,7 +15,7 @@ fun Project.configureGradleIntellijPlugin(packageSearchExtension: PackageSearchE
 
     plugins.withId("org.jetbrains.intellij") {
         extensions.withType<IntelliJPluginExtension> {
-            version = "233.11799.241"
+            version = "233-EAP-SNAPSHOT"
             instrumentCode = false
             downloadSources = !isCI
         }
