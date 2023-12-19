6d4e7d4ff98d0f2a44f51892b6c2a5c18524d678
fscarponi <fscarponi@gmail.com>
2023-12-19 16:41:15 +0100

    Rollback to Compose Foundation SplitPane, waiting fix for the jewel's one

--- plugin/src/main/kotlin/com/jetbrains/packagesearch/plugin/ui/PackageSearchPackagePanel.kt
+++ plugin/src/main/kotlin/com/jetbrains/packagesearch/plugin/ui/PackageSearchPackagePanel.kt
@@ -1,13 +1,23 @@
 package com.jetbrains.packagesearch.plugin.ui
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.intellij.ui.JBColor
 import com.jetbrains.packagesearch.plugin.core.data.PackageSearchModule
+import com.jetbrains.packagesearch.plugin.ui.bridge.defaultPKGSSplitter
 import com.jetbrains.packagesearch.plugin.ui.model.packageslist.PackageListItemEvent
 import com.jetbrains.packagesearch.plugin.ui.panels.packages.PackageSearchCentralPanel
 import com.jetbrains.packagesearch.plugin.ui.panels.side.PackageSearchInfoPanel
 import com.jetbrains.packagesearch.plugin.ui.panels.tree.PackageSearchModulesTree
-import org.jetbrains.jewel.ui.component.HorizontalSplitLayout
+import org.jetbrains.compose.splitpane.HorizontalSplitPane
+import org.jetbrains.compose.splitpane.rememberSplitPaneState
+import org.jetbrains.jewel.bridge.toComposeColor
+import org.jetbrains.jewel.foundation.theme.JewelTheme
 
 @Composable
 fun PackageSearchPackagePanel(
@@ -16,19 +26,29 @@ fun PackageSearchPackagePanel(
     onLinkClick: (String) -> Unit,
     onPackageEvent: (PackageListItemEvent) -> Unit,
 ) {
-    HorizontalSplitLayout(
-        first = { PackageSearchModulesTree(it, onSelectionModulesSelectionChanged) },
-        second = {
+    val splitPaneState = rememberSplitPaneState(PackageSearchMetrics.Splitpane.firstSplitterPositionPercentage )
+    val innerSplitPaneState = rememberSplitPaneState(PackageSearchMetrics.Splitpane.secondSplittePositionPercentage)
+    val splitterColor by remember(JewelTheme.isDark) { mutableStateOf(JBColor.border().toComposeColor()) }
+
+    HorizontalSplitPane(Modifier.fillMaxSize(), splitPaneState) {
+        first {
+            PackageSearchModulesTree(Modifier, onSelectionModulesSelectionChanged)
+        }
+        defaultPKGSSplitter(splitterColor)
+        second {
             if (isInfoPanelOpen) {
-                HorizontalSplitLayout(
-                    modifier = it,
-                    initialDividerPosition = 700.dp,
-                    first = { PackageSearchCentralPanel(it, onLinkClick) },
-                    second = { PackageSearchInfoPanel(it, onLinkClick, onPackageEvent) },
-                    maxRatio = 0.8f
-                )
-            } else PackageSearchCentralPanel(it, onLinkClick)
-        },
-        minRatio = 0.1f,
-    )
+                HorizontalSplitPane(Modifier.fillMaxSize(), innerSplitPaneState) {
+                    first {
+                        PackageSearchCentralPanel(onLinkClick = onLinkClick)
+                    }
+                    defaultPKGSSplitter(splitterColor)
+                    second(300.dp) {
+                        PackageSearchInfoPanel(onLinkClick = onLinkClick, onPackageEvent = onPackageEvent)
+                    }
+                }
+
+            } else PackageSearchCentralPanel(onLinkClick = onLinkClick)
+        }
+    }
 }
+
