3d477a2d96dede580c62fec536b9e8b26493c9d5
fscarponi <fscarponi@gmail.com>
2024-01-10 17:11:34 +0100

    Fix Jewel and Compose dependencies, align code to jewel 0.12

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -49,6 +49,12 @@ val tooling: Configuration by configurations.creating {
 }
 
 dependencies {
+    implementation(compose.desktop.linux_arm64)
+    implementation(compose.desktop.linux_x64)
+    implementation(compose.desktop.macos_arm64)
+    implementation(compose.desktop.macos_x64)
+    implementation(compose.desktop.windows_x64)
+    implementation(packageSearchCatalog.jewel.bridge.ij233)
     implementation(packageSearchCatalog.kotlinx.serialization.core)
     implementation(packageSearchCatalog.compose.desktop.components.splitpane){
         exclude(group = "org.jetbrains.compose.runtime")
