eeb4e093a1fcc12e5db72a1f48a8097eb7501b2c
Lamberto Basti <basti.lamberto@gmail.com>
2024-02-07 13:37:44 +0100

    Added default headers in PKGS APIs calls for IDE version and PKGS plugin version.

--- plugin/src/main/kotlin/com/jetbrains/packagesearch/plugin/services/PackageSearchApplicationCachesService.kt
+++ plugin/src/main/kotlin/com/jetbrains/packagesearch/plugin/services/PackageSearchApplicationCachesService.kt
@@ -6,12 +6,15 @@ import com.intellij.ide.actions.cache.AsyncRecoveryResult
 import com.intellij.ide.actions.cache.RecoveryAction
 import com.intellij.ide.actions.cache.RecoveryScope
 import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.ApplicationInfo
 import com.intellij.openapi.application.appSystemDir
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.Service.Level
+import com.intellij.openapi.components.service
 import com.jetbrains.packagesearch.plugin.PackageSearchBundle
 import com.jetbrains.packagesearch.plugin.core.PackageSearch
 import com.jetbrains.packagesearch.plugin.core.nitrite.buildDefaultNitrate
+import com.jetbrains.packagesearch.plugin.core.utils.IntelliJApplication
 import com.jetbrains.packagesearch.plugin.core.utils.PKGSInternalAPI
 import com.jetbrains.packagesearch.plugin.utils.ApiPackageCacheEntry
 import com.jetbrains.packagesearch.plugin.utils.ApiRepositoryCacheEntry
@@ -20,8 +23,10 @@ import com.jetbrains.packagesearch.plugin.utils.KtorDebugLogger
 import com.jetbrains.packagesearch.plugin.utils.PackageSearchApiPackageCache
 import com.jetbrains.packagesearch.plugin.utils.PackageSearchProjectService
 import io.ktor.client.engine.java.Java
+import io.ktor.client.plugins.DefaultRequest
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logging
+import io.ktor.http.headers
 import java.util.concurrent.CompletableFuture
 import kotlin.io.path.absolutePathString
 import kotlin.io.path.div
@@ -83,6 +88,12 @@ class PackageSearchApplicationCachesService : RecoveryAction, Disposable {
                 level = LogLevel.ALL
                 logger = KtorDebugLogger()
                 filter { it.attributes.getOrNull(PackageSearchApiClient.Attributes.Cache) == true }
+            }
+            install(DefaultRequest) {
+                headers {
+                    append("JB-Plugin-Version", PackageSearch.pluginVersion)
+                    append("JB-IDE-Version", IntelliJApplication.service<ApplicationInfo>().strictVersion)
+                }
             }
         }
     )
