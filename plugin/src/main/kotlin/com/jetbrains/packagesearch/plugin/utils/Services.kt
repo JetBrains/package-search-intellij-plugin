24a7ec7fdc59abad17a8b3d072b1aa2f9d000a0f
Lamberto Basti <basti.lamberto@gmail.com>
2024-02-27 14:57:06 +0100

    Refactor logging system and adjust FUSEvent handling, fix IDEA-347208
    The logging system has been refactored to log events using a new PackageSearchFUSEvent sealed interface, to handle logging more uniformly across the code. Furthermore, the sharing behavior of selectedModulesFlow in PackageListViewModel has been adjusted for better performance and readability. These changes help improve the organization and readability of the code while maintaining its functionality.

--- plugin/src/main/kotlin/com/jetbrains/packagesearch/plugin/utils/Services.kt
+++ plugin/src/main/kotlin/com/jetbrains/packagesearch/plugin/utils/Services.kt
@@ -3,7 +3,10 @@ package com.jetbrains.packagesearch.plugin.utils
 import com.intellij.openapi.application.Application
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
+import com.jetbrains.packagesearch.plugin.core.utils.IntelliJApplication
+import com.jetbrains.packagesearch.plugin.fus.PackageSearchFUSEvent
 import com.jetbrains.packagesearch.plugin.services.PackageSearchApplicationCachesService
+import com.jetbrains.packagesearch.plugin.services.PackageSearchFUSService
 import com.jetbrains.packagesearch.plugin.services.PackageSearchProjectService
 
 
@@ -12,3 +15,9 @@ val Application.PackageSearchApplicationCachesService
 
 val Project.PackageSearchProjectService
     get() = service<PackageSearchProjectService>()
+
+val Application.PackageSearchFUSService
+    get() = service<PackageSearchFUSService>()
+
+internal fun logFUSEvent(event: PackageSearchFUSEvent) =
+    IntelliJApplication.PackageSearchFUSService.logEvent(event)
