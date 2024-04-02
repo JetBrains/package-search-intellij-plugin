27c7d5ea43b2616d750d5b0a8a4ea590cdf0fb63
fscarponi <fscarponi@gmail.com>
2024-04-02 14:15:09 +0200

    Refactor test class names and related workflow settings
    Rename `MavenProjectsTest` to `CatalogProjectTest`, adjust its resource path accordingly, and reflect this change in the Github workflow file. Rename `VersionCatalogTest` to `MavenProjectTest` and update its resource path, achieving a better alignment with the functionality being tested.

--- plugin/src/test/kotlin/com/jetbrains/packagesearch/plugin/tests/projectservice/MavenProjectTest.kt
+++ plugin/src/test/kotlin/com/jetbrains/packagesearch/plugin/tests/projectservice/MavenProjectTest.kt
@@ -3,8 +3,8 @@ package com.jetbrains.packagesearch.plugin.tests.projectservice
 import org.junit.jupiter.api.TestInstance
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class VersionCatalogTest : PackageSearchProjectServiceTest() {
+class MavenProjectTest : PackageSearchProjectServiceTest() {
 
-    override val resourcePath = "/projects/catalog/"
+    override val resourcePath = "/projects/maven/"
 
 }
