50dd483c8c5b2c0d5c6c9b9c16b86d78eaa430df
fabrizio.scarponi <fscarponi@gmail.com>
2024-11-08 17:01:23 +0100

    fixed by shadowing

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -33,15 +33,32 @@ dependencies {
             "org.jetbrains.idea.reposearch",
             "com.jetbrains.performancePlugin",
         )
-        bundledModule(
-            "intellij.platform.compose"
-        )
+//        bundledModule(
+//            "intellij.platform.compose"
+//        )
     }
 
-    implementation(compose.desktop.currentOs) {
+    implementation(compose.desktop.linux_x64) {
         exclude(group = "org.jetbrains.compose.material")
         exclude(group = "org.jetbrains.kotlinx")
     }
+    implementation(compose.desktop.linux_arm64) {
+        exclude(group = "org.jetbrains.compose.material")
+        exclude(group = "org.jetbrains.kotlinx")
+    }
+    implementation(compose.desktop.windows_x64) {
+        exclude(group = "org.jetbrains.compose.material")
+        exclude(group = "org.jetbrains.kotlinx")
+    }
+    implementation(compose.desktop.macos_arm64) {
+        exclude(group = "org.jetbrains.compose.material")
+        exclude(group = "org.jetbrains.kotlinx")
+    }
+    implementation(compose.desktop.macos_x64) {
+        exclude(group = "org.jetbrains.compose.material")
+        exclude(group = "org.jetbrains.kotlinx")
+    }
+
     implementation(packageSearchCatalog.jewel.bridge.ij243) //compileonly???
     implementation(packageSearchCatalog.kotlinx.serialization.core)
     implementation(packageSearchCatalog.compose.desktop.components.splitpane) {
@@ -97,6 +114,10 @@ tasks {
         exclude { it.name.containsAny(JAR_NAMES_TO_REMOVE) }
         exclude { it.name == "module-info.class" }
         exclude { it.name.endsWith("kotlin_module") }
+        relocate("androidx.*", "shadow.androidx.*")
+        relocate("org.jetbrains.jewel.*", "shadow.org.jetbrains.jewel.*")
+        relocate("org.jetbrains.compose.*", "shadow.org.jetbrains.compose.*")
+        relocate("com.jetbrains.compose.*", "shadow.com.jetbrains.compose.*")
     }
 
     val buildShadowPlugin by registering(Zip::class) {
